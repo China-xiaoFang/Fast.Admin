5573476b538b18e46f0a00d4dddea6ef2ac5065e
小方 <2875616188@qq.com>
2025-11-06 14:46:03 +0800

    登录页面。

--- App.Admin/src/common/router.ts
+++ App.Admin/src/common/router.ts
@@ -2,37 +2,29 @@
  * @description 常用路由
  */
 export const CommonRoute = {
-	/**
-	 * App加载页
-	 */
+	/** App加载页 */
 	Launcher: "/pages/launcher/index",
-	/**
-	 * WebView页面
-	 */
+	/** WebView页面 */
 	WebView: "/pages/webView/index",
-	/**
-	 * 登录页
-	 */
+	/** 用户协议 */
+	UserAgreement: "/pages/agreement/user/index",
+	/** 隐私协议 */
+	PrivacyAgreement: "/pages/agreement/privacy/index",
+	/** 服务协议 */
+	ServiceAgreement: "/pages/agreement/service/index",
+	/** 登录页 */
 	Login: "/pages/login/index",
-	/**
-	 * 选择租户
-	 */
+	/** 选择租户 */
 	SelectTenant: "/pages/selectTenant/index",
-	/**
-	 * 首页
-	 */
+	/** 首页 */
 	Home: "/pages/tabBar/home/index",
-	/**
-	 * 工作台
-	 */
+	/** 工作台 */
 	Workbench: "/pages/tabBar/workbench/index",
-	/**
-	 * 我的
-	 */
+	/** 我的 */
 	My: "/pages/tabBar/my/index",
 };
 
 /**
- * TabBar 路由
+ * @description TabBar 路由
  */
 export const TabBarRoute = [CommonRoute.Home, CommonRoute.Workbench, CommonRoute.My];
