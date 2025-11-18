0ab03e9d7d8e20a09bfdc7986cbeab1a6b3644b6
小方 <2875616188@qq.com>
2025-11-18 22:13:52 +0800

    文件头注释。

--- Api.Server/src/Domain/Core/Handlers/GlobalExceptionHandler.cs
+++ Api.Server/src/Domain/Core/Handlers/GlobalExceptionHandler.cs
@@ -1,4 +1,26 @@
-﻿using System.Net.Sockets;
+﻿// ------------------------------------------------------------------------
+// Apache开源许可证
+// 
+// 版权所有 © 2018-Now 小方
+// 
+// 许可授权：
+// 本协议授予任何获得本软件及其相关文档（以下简称“软件”）副本的个人或组织。
+// 在遵守本协议条款的前提下，享有使用、复制、修改、合并、发布、分发、再许可、销售软件副本的权利：
+// 1.所有软件副本或主要部分必须保留本版权声明及本许可协议。
+// 2.软件的使用、复制、修改或分发不得违反适用法律或侵犯他人合法权益。
+// 3.修改或衍生作品须明确标注原作者及原软件出处。
+// 
+// 特别声明：
+// - 本软件按“原样”提供，不提供任何形式的明示或暗示的保证，包括但不限于对适销性、适用性和非侵权的保证。
+// - 在任何情况下，作者或版权持有人均不对因使用或无法使用本软件导致的任何直接或间接损失的责任。
+// - 包括但不限于数据丢失、业务中断等情况。
+// 
+// 免责条款：
+// 禁止利用本软件从事危害国家安全、扰乱社会秩序或侵犯他人合法权益等违法活动。
+// 对于基于本软件二次开发所引发的任何法律纠纷及责任，作者不承担任何责任。
+// ------------------------------------------------------------------------
+
+using System.Net.Sockets;
 using System.Text;
 using System.Text.RegularExpressions;
 using Fast.CenterLog.Entity;
@@ -10,7 +32,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using SqlSugar;
 using Yitter.IdGenerator;
-
 
 namespace Fast.Core;
 
@@ -45,8 +66,7 @@ public class GlobalExceptionHandler : IGlobalExceptionHandler
     /// <param name="isUserFriendlyException"><see cref="T:System.Boolean" /> 是否友好异常</param>
     /// <param name="isValidationException"><see cref="T:System.Boolean" /> 是否验证异常</param>
     /// <returns></returns>
-    public async Task OnExceptionAsync(ExceptionContext context, bool isUserFriendlyException,
-        bool isValidationException)
+    public async Task OnExceptionAsync(ExceptionContext context, bool isUserFriendlyException, bool isValidationException)
     {
         var httpContext = context.HttpContext;
         var message = new StringBuilder();
@@ -122,8 +142,7 @@ public class GlobalExceptionHandler : IGlobalExceptionHandler
         if (isUserFriendlyException)
         {
             // 只写入最深的一条堆栈信息
-            var firstLine = context.Exception.StackTrace
-                ?.Split([Environment.NewLine], StringSplitOptions.RemoveEmptyEntries)
+            var firstLine = context.Exception.StackTrace?.Split([Environment.NewLine], StringSplitOptions.RemoveEmptyEntries)
                 .FirstOrDefault();
 
             // 如果有匹配的堆栈信息，选择第一条（最深的那一条）
