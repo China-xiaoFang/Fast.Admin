a5e515690027b899f4e9918b32d4d776ffad6d7b
小方 <2875616188@qq.com>
2025-11-20 04:23:34 +0800

    1

--- App.Admin/src/pages/setting/index.vue
+++ App.Admin/src/pages/setting/index.vue
@@ -10,8 +10,8 @@
 			<!-- #ifdef APP-PLUS -->
 			<wd-cell title="热更新版本" :value="`v${appStore.appVersion}`" />
 			<!-- #endif -->
-			<wd-cell title="系统服务商" value="FastDotNet" clickable isLink />
-			<wd-cell customClass="mb20" title="服务有效期" value="2029-12-31 23:59:59" clickable isLink />
+			<wd-cell title="系统服务商" value="FastDotNet" />
+			<wd-cell customClass="mb20" title="服务有效期" value="2029-12-31 23:59:59" />
 		</wd-cell-group>
 
 		<wd-cell-group border>
