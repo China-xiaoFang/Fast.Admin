48d2ef79a2908dfb523fda7b2718723e34c7198d
小方 <2875616188@qq.com>
2025-11-20 04:00:35 +0800

    调度作业。

---
+++ App.Admin/src/api/enums/MailMessageEnum.ts
@@ -0,0 +1,29 @@
+/**
+ * Fast.Scheduler.MailMessageEnum 邮件消息枚举
+ */
+export enum MailMessageEnum {
+  /**
+   * 无
+   */
+  None = 0,
+  /**
+   * 信息
+   */
+  Info = 1,
+  /**
+   * 警告
+   */
+  Warn = 2,
+  /**
+   * 错误
+   */
+  Error = 4,
+  /**
+   * WarnAndError
+   */
+  WarnAndError = 6,
+  /**
+   * All
+   */
+  All = 7,
+}
---
+++ App.Admin/src/api/enums/TriggerTypeEnum.ts
@@ -0,0 +1,17 @@
+/**
+ * Fast.Scheduler.TriggerTypeEnum 触发器类型枚举
+ */
+export enum TriggerTypeEnum {
+  /**
+   * Cron
+   */
+  Cron = 1,
+  /**
+   * Daily
+   */
+  Daily = 2,
+  /**
+   * Simple
+   */
+  Simple = 4,
+}
