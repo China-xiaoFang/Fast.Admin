eb770d344216399ac8a7cb5a5c1e52e2ed3a611f
小方 <2875616188@qq.com>
2025-11-20 04:00:35 +0800

    调度作业。

---
+++ Web.Admin/src/api/services/scheduler/models/SchedulerJobKeyInput.ts
@@ -0,0 +1,16 @@
+import { SchedulerJobGroupEnum } from "@/api/enums/SchedulerJobGroupEnum";
+
+/**
+ * Fast.Scheduler.SchedulerJobKeyInput 调度作业Key输入
+ */
+export interface SchedulerJobKeyInput {
+  /**
+   * 作业名称
+   */
+  jobName?: string;
+  /**
+   * 
+   */
+  jobGroup?: SchedulerJobGroupEnum;
+}
+
