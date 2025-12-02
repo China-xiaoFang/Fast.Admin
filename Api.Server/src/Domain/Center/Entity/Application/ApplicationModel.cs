bbca131db2ac01f3ea994ff275ad95c7ee997668
小方 <2875616188@qq.com>
2025-12-02 20:57:37 +0800

    1

--- Api.Server/src/Domain/Center/Entity/Application/ApplicationModel.cs
+++ Api.Server/src/Domain/Center/Entity/Application/ApplicationModel.cs
@@ -118,7 +118,7 @@ public class ApplicationModel : BaseEntity, IUpdateVersion
     /// <summary>
     /// 租户名称
     /// </summary>
-    [SugarColumn(ColumnDescription = "租户名称", Length = 30)]
+    [SugarColumn(ColumnDescription = "租户名称", Length = 30, CreateTableFieldSort = 997)]
     public string TenantName { get; set; }
 
     /// <summary>
