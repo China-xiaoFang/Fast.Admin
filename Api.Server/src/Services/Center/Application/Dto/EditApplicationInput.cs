2fb5f036ad9620973b654d104ea39a1b057a9992
小方 <2875616188@qq.com>
2025-12-04 11:01:01 +0800

    应用增加经纬度

--- Api.Server/src/Services/Center/Application/Dto/EditApplicationInput.cs
+++ Api.Server/src/Services/Center/Application/Dto/EditApplicationInput.cs
@@ -168,14 +168,19 @@ public class EditApplicationInput : UpdateVersionInput
         public string ContactPhone { get; set; }
 
         /// <summary>
+        /// 纬度
+        /// </summary>
+        public decimal? Latitude { get; set; }
+
+        /// <summary>
         /// 经度
         /// </summary>
         public decimal? Longitude { get; set; }
 
         /// <summary>
-        /// 纬度
+        /// 地址
         /// </summary>
-        public decimal? Latitude { get; set; }
+        public string Address { get; set; }
 
         /// <summary>
         /// Banner图
