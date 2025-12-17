2329e37c800c1d163450c2ff9a591cbe80c135fd
小方 <2875616188@qq.com>
2025-12-18 01:31:05 +0800

    调度程序

--- Web.Admin/src/components/index.ts
+++ Web.Admin/src/components/index.ts
@@ -1,7 +1,7 @@
-import type { DefineComponent } from "vue";
 import Editor from "./Editor/index.vue";
 import Footer from "./Footer/index.vue";
 import Loading from "./Loading/index.vue";
+import Table from "./Table/index.tsx";
 import Tag from "./Tag/index.vue";
 import Tendril from "./Tendril/index.vue";
 import Watermark from "./Watermark/index.vue";
@@ -9,15 +9,9 @@ import Watermark from "./Watermark/index.vue";
 export * from "./Editor/index.vue";
 export * from "./Footer/index.vue";
 export * from "./Loading/index.vue";
+export * from "./Table/index.tsx";
 export * from "./Tag/index.vue";
 export * from "./Tendril/index.vue";
 export * from "./Watermark/index.vue";
 
-export default [
-	Editor,
-	Footer,
-	Loading,
-	Tag,
-	Tendril,
-	Watermark,
-] as unknown as DefineComponent[];
+export default [Editor, Footer, Loading, Table, Tag, Tendril, Watermark];
