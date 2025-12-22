14a486e83fc1e6763209a3971ac292eda8302a3a
小方 <2875616188@qq.com>
2025-12-22 21:29:27 +0800

    数据库配置。

--- Api.Server/src/Services/Center/Database/Dto/QueryDatabasePagedInput.cs
+++ Api.Server/src/Services/Center/Database/Dto/QueryDatabasePagedInput.cs
@@ -4,14 +4,14 @@
 // 版权所有 © 2018-Now 小方
 // 
 // 许可授权：
-// 本协议授予任何获得本软件及其相关文档（以下简称"软件"）副本的个人或组织。
+// 本协议授予任何获得本软件及其相关文档（以下简称“软件”）副本的个人或组织。
 // 在遵守本协议条款的前提下，享有使用、复制、修改、合并、发布、分发、再许可、销售软件副本的权利：
 // 1.所有软件副本或主要部分必须保留本版权声明及本许可协议。
 // 2.软件的使用、复制、修改或分发不得违反适用法律或侵犯他人合法权益。
 // 3.修改或衍生作品须明确标注原作者及原软件出处。
 // 
 // 特别声明：
-// - 本软件按"原样"提供，不提供任何形式的明示或暗示的保证，包括但不限于对适销性、适用性和非侵权的保证。
+// - 本软件按“原样”提供，不提供任何形式的明示或暗示的保证，包括但不限于对适销性、适用性和非侵权的保证。
 // - 在任何情况下，作者或版权持有人均不对因使用或无法使用本软件导致的任何直接或间接损失的责任。
 // - 包括但不限于数据丢失、业务中断等情况。
 // 
@@ -20,57 +20,25 @@
 // 对于基于本软件二次开发所引发的任何法律纠纷及责任，作者不承担任何责任。
 // ------------------------------------------------------------------------
 
-using Fast.Center.Enum;
-
 namespace Fast.Center.Service.Database.Dto;
 
 /// <summary>
-/// <see cref="QueryMainDatabaseDetailOutput"/> 获取主库模板详情输出
+/// <see cref="QueryDatabasePagedInput"/> 获取数据库分页列表输入
 /// </summary>
-public class QueryMainDatabaseDetailOutput : PagedOutput
+public class QueryDatabasePagedInput : PagedInput
 {
-    /// <summary>
-    /// 主库模板Id
-    /// </summary>
-    public long MainDatabaseId { get; set; }
-
-    /// <summary>
-    /// 数据库名称
-    /// </summary>
-    public string DatabaseName { get; set; }
-
     /// <summary>
     /// 数据库类型
     /// </summary>
-    public DbType DbType { get; set; }
+    public DatabaseTypeEnum? DatabaseType { get; set; }
 
     /// <summary>
-    /// 主机地址
+    /// 数据库类型，用于区分使用的是那个类型的数据库
     /// </summary>
-    public string Host { get; set; }
+    public DbType? DbType { get; set; }
 
     /// <summary>
-    /// 端口
+    /// 租户Id
     /// </summary>
-    public int Port { get; set; }
-
-    /// <summary>
-    /// 用户名
-    /// </summary>
-    public string UserName { get; set; }
-
-    /// <summary>
-    /// 密码
-    /// </summary>
-    public string Password { get; set; }
-
-    /// <summary>
-    /// 状态
-    /// </summary>
-    public CommonStatusEnum Status { get; set; }
-
-    /// <summary>
-    /// 备注
-    /// </summary>
-    public string Remark { get; set; }
+    public long? TenantId { get; set; }
 }
