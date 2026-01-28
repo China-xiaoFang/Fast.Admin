ba0de22745ee790dc99644633b1b09ad59cb396d
小方 <2875616188@qq.com>
2026-01-28 20:37:37 +0800

    1

--- Web.Admin/src/views/system/tenantComplaint/index.vue
+++ Web.Admin/src/views/system/tenantComplaint/index.vue
@@ -1,6 +1,10 @@
 <template>
 	<div>
 		<FastTable ref="fastTableRef" tableKey="1D1KQSM6QW" rowKey="complaintId" :requestApi="complaintApi.queryTenantComplaintPaged" hideSearchTime>
+			<template #attachmentImages="{ row }: { row?: QueryComplaintPagedOutput }">
+				<el-button size="small" plain @click="state.previewSrcList = row.attachmentImages"> 查看 </el-button>
+			</template>
+
 			<!-- 表格操作 -->
 			<template #operation="{ row }: { row: QueryComplaintPagedOutput }">
 				<el-button
@@ -16,11 +20,19 @@
 			</template>
 		</FastTable>
 		<TenantComplaintEdit ref="editFormRef" @ok="fastTableRef.refresh()" />
+		<el-image-viewer
+			v-if="state.previewSrcList.length > 0"
+			:urlList="state.previewSrcList"
+			hideOnClickModal
+			teleported
+			showProgress
+			@close="state.previewSrcList = []"
+		/>
 	</div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 import { complaintApi } from "@/api/services/Center/complaint";
 import { QueryComplaintPagedOutput } from "@/api/services/Center/complaint/models/QueryComplaintPagedOutput";
 import TenantComplaintEdit from "./edit/index.vue";
@@ -32,4 +44,8 @@ defineOptions({
 
 const fastTableRef = ref<FastTableInstance>();
 const editFormRef = ref<InstanceType<typeof TenantComplaintEdit>>();
+
+const state = reactive({
+	previewSrcList: [],
+});
 </script>
