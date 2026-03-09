61156ad76b7a8f4df659f430b09daa5836ef4912
小方 <2875616188@qq.com>
2026-03-09 17:50:11 +0800

    水平菜单。

---
+++ Web.Admin/src/layouts/LayoutHorizontal/components/menuItem.vue
@@ -0,0 +1,51 @@
+<template>
+	<el-sub-menu v-if="props.menu.children?.length > 0" :index="props.menu.children[0].router" :title="props.menu.menuTitle || props.menu.menuName">
+		<template #title>
+			<FaIcon v-if="props.menu.icon" :name="props.menu.icon" />
+			<span>{{ props.menu.menuName }}</span>
+		</template>
+		<MenuItem v-for="(item, idx) in props.menu.children" :key="idx" :menu="item" />
+	</el-sub-menu>
+	<el-menu-item v-else :index="props.menu.router || props.menu.link" @click="handleMenuClick">
+		<FaIcon v-if="props.menu.icon" :name="props.menu.icon" />
+		<template #title>
+			<span>{{ props.menu.menuName }}</span>
+		</template>
+	</el-menu-item>
+</template>
+
+<script setup lang="ts">
+import { definePropType } from "@fast-china/utils";
+import { useRouter } from "vue-router";
+import { MenuTypeEnum } from "@/api/enums/MenuTypeEnum";
+import MenuItem from "./menuItem.vue";
+import type { AuthMenuInfoDto } from "@/api/services/Auth/auth/models/AuthMenuInfoDto";
+
+defineOptions({
+	name: "LayoutHorizontalMenuItem",
+});
+
+const router = useRouter();
+
+const props = defineProps({
+	/** 菜单 */
+	menu: definePropType<AuthMenuInfoDto>(Object),
+});
+
+const handleMenuClick = () => {
+	switch (props.menu.menuType) {
+		case MenuTypeEnum.Catalog:
+		case MenuTypeEnum.Menu:
+			break;
+		case MenuTypeEnum.Internal:
+			router.push({
+				path: "/iframe",
+				query: { url: props.menu.link },
+			});
+			break;
+		case MenuTypeEnum.Outside:
+			window.open(props.menu.link, "_blank");
+			break;
+	}
+};
+</script>
