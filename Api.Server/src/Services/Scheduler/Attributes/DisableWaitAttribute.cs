e5ea1ffb64f6b046d4443b9469eb1b581e7dfb30
小方 <2875616188@qq.com>
2026-03-10 00:34:22 +0800

    一些错误调整。

--- Api.Server/src/Services/Scheduler/Attributes/DisableWaitAttribute.cs
+++ Api.Server/src/Services/Scheduler/Attributes/DisableWaitAttribute.cs
@@ -26,6 +26,7 @@ namespace Fast.Scheduler;
 /// <see cref="DisableWaitAttribute"/> 禁用等待
 /// </summary>
 /// <remarks>针对 LocalJob 且是全部租户作业的，禁用随机等待操作</remarks>
+[SuppressSniffer]
 [AttributeUsage(AttributeTargets.Class)]
 public class DisableWaitAttribute : Attribute
 {
