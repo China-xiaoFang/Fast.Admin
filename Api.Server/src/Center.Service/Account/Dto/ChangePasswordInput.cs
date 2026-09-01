aac34cde1fafe011b492b5733292e04966b9835f
小方 <2875616188@qq.com>
2026-09-01 20:02:42 +0800

    密码处理。

--- Api.Server/src/Center.Service/Account/Dto/ChangePasswordInput.cs
+++ Api.Server/src/Center.Service/Account/Dto/ChangePasswordInput.cs
@@ -30,20 +30,20 @@ public class ChangePasswordInput : UpdateVersionInput
     /// <summary>
     /// 旧密码
     /// </summary>
-    [StringRequired(ErrorMessage = "旧密码不能为空"), MaxLength(512, ErrorMessage = "旧密码不能超过512位字符")]
+    [StringRequired(ErrorMessage = "旧密码不能为空")]
     public string OldPassword { get; set; }
 
     /// <summary>
     /// 新密码
     /// </summary>
-    [StringRequired(ErrorMessage = "新密码不能为空"), MinLength(6, ErrorMessage = "新密码不能少于6位字符"),
+    [StringRequired(ErrorMessage = "新密码不能为空"), MinLength(8, ErrorMessage = "新密码不能少于8位字符"),
      MaxLength(20, ErrorMessage = "新密码不能超过20位字符")]
     public string NewPassword { get; set; }
 
     /// <summary>
     /// 确认密码
     /// </summary>
-    [StringRequired(ErrorMessage = "确认密码不能为空"), MinLength(6, ErrorMessage = "确认密码不能少于6位字符"),
+    [StringRequired(ErrorMessage = "确认密码不能为空"), MinLength(8, ErrorMessage = "确认密码不能少于8位字符"),
      MaxLength(20, ErrorMessage = "确认密码不能超过20位字符")]
     public string ConfirmPassword { get; set; }
 }
--- Web.Admin/src/views/system/database/edit/index.vue
+++ Web.Admin/src/views/system/database/edit/index.vue
@@ -44,6 +44,7 @@
 					v-model="state.formData.dbPwd"
 					show-password
 					maxlength="20"
+					show-word-limit
 					placeholder="请输入数据库密码"
 					autocomplete="new-password"
 				/>
@@ -150,6 +151,7 @@
 									type="password"
 									v-model="row.dbPwd"
 									show-password
+									show-word-limit
 									maxlength="20"
 									placeholder="请输入数据库密码"
 									autocomplete="new-password"
